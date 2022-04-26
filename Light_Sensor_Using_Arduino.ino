dae76c268c7687090a802e1ab2c9afa4f753579f
John bourmpoulas <46540325+JohnBourmpoulas@users.noreply.github.com>
2022-04-27 00:00:50 +0300

    Add files via upload

---
+++ Light_Sensor_Using_Arduino.ino
@@ -0,0 +1,26 @@
+const int ledPin = 3;
+const int ldrPin = A0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+}
+
+void loop() {
+  int ldrStatus = analogRead(ldrPin);
+
+  if (ldrStatus <= 125)
+  {
+    digitalWrite(ledPin, HIGH);
+    Serial.print("Its Dark, Turn on the LED:");
+    Serial.println(ldrStatus);
+
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+    Serial.print("Its Bright, Turn off the LED:");
+    Serial.println(ldrStatus);
+  }
+
+}
